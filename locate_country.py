1130f2bbac7effc38068349eeb38daf822fe20ff
chrjoh <christerjoha@gmail.com>
2019-09-10 09:16:55 +0200

    change print output

--- locate_country.py
+++ locate_country.py
@@ -10,6 +10,7 @@ if len(sys.argv) < 2:
     exit(1)
 
 reader = geoip2.database.Reader('./GeoLite2-Country.mmdb')
+ano_reader = geoip2.database.Reader('./GeoIP2-Anonymous-IP.mmdb')
 
 def read_ip_list():
     ip_file = sys.argv[1]
@@ -41,6 +42,19 @@ def get_ip_country(ip):
         sys.stderr.write('Could not retrieve any geolocation for IP {}\n'.format(ip))
         return False
 
+
+def get_anonymous(ip):
+    response = ano_reader.anonymous_ip(ip)
+    if response.is_anonymous:
+        return {
+                'vpn': response.is_anonymous_vpn,
+                'hosting_provider': response.is_hosting_provider,
+                'public_proxy': response.is_public_proxy,
+                'tor_exit_node': response.is_tor_exit_node,
+            }
+    else:
+        return False
+
 countries = dict()
 ip_list = read_ip_list()
 for ip in ip_list:
@@ -51,7 +65,11 @@ for ip in ip_list:
             countries[key] += 1
         else:
             countries[key] = 1
-        #print("{},{},{}".format(ip, result['country_code'].encode('utf-8'), result['country_name'].encode('utf-8')))
+        anonymous = get_anonymous(ip)
+        if anonymous:
+            print("{},{},{},{}".format(ip, result['country_code'].encode('utf-8'), result['country_name'].encode('utf-8'),anonymous))
+        else:   
+            print("{},{},{}".format(ip, result['country_code'].encode('utf-8'), result['country_name'].encode('utf-8')))
 
 sorted_countries = sorted(countries.items(), key=operator.itemgetter(1))
 for value in sorted_countries:
